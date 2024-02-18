306745022967ce2fc47fe745e39d94e6b477a625
developerambrish <160359651+developerambrish@users.noreply.github.com>
2024-02-18 11:58:37 +0000

    type conversion

---
+++ basic/conversion_operation.js
@@ -0,0 +1,32 @@
+let number = "33asa" // change the value here like 22,null undefind true false
+// console.log(typeof number);
+// console.log(typeof(number));
+let string = Number(number)
+//console.log(typeof string);
+//console.log(string);
+
+
+/* 
+if we are converting into number 
+"33" =33 converted successfullly
+"33sd" == conveted but value NaN(not a number)
+null = o
+true = 1 ; false =0
+undefined  = Nan
+*/
+
+let score ="sa121"
+let convertintostring = String (score)
+console.log(typeof convertintostring);
+console.log(convertintostring);
+
+/* 
+if we are convertiing into string
+
+12 = converted
+null  = converted & value is null
+undefined = converted value is undefined
+true = converted value is true
+asd = error
+"asaas12" succeessfully
+*/
