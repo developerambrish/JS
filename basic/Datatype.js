282f990efcd9212472afc5892c5ff8f08f04fcef
developerambrish <160359651+developerambrish@users.noreply.github.com>
2024-02-18 11:58:01 +0000

    datatype

---
+++ basic/Datatype.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js as newer version of js not old
+// alert(3+ 3) its working with brower not node js
+
+//privitive datatype
+
+//number confused into int & number
+//bigint
+//string
+//Boolean = true & false
+//null = standalone vale // this is object type
+//undefined
+//symbol = unique
+
+
+//object
+
+console.log(typeof null); //obj 
+console.log(typeof undefined); //undefined
